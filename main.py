a27521486013bb80b196cc064498af2395d0c036
Marcelo Almeida <marcelo_almeida@live.com>
2023-05-01 17:36:44 -0300

    Readme e ajustes finais

--- main.py
+++ main.py
@@ -11,16 +11,13 @@ Astro.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Dependen
 origins = ["http://localhost",    "http://localhost:5500",
            "http://127.0.0.1",    "http://127.0.0.1:5500"]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
 )
 
 
